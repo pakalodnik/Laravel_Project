23107ff658e8717e09ce8ebc7ecdc43c9a1b1241
Sulta <sultan.kz19991@gmail.com>
2020-04-21 01:21:02 +0600

    Rave v rot

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,37 +1,11 @@
-@extends('layouts')
-@section('content')
-<div class="container">
-    <div class="row">
-        <h3>Rava Down tasks</h3>
-        <a href="{{route('tasks.create')}}" class="btn btn-success"></a>
-        <div class="col-md-10 col-md-offset-1">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>Title</td>
-                        <td>Actions</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>My title</td>
-                        <td>
-                            <a href="#">
-                                <i class="glyphicon glyphicon-eye-open"></i>
-                            </a>
-                            <a href="#">
-                                <i class="glyphicon glyphicon-edit"></i>
-                            </a>
-                            <a href="#">
-                                <i class="glyphicon glyphicon-remove"></i>
-                            </a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>APP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
